77cb93d0c79e4a3404d95e1c9339659bf0d8349f
YannisOuld <yannisould.dev@outlook.fr>
2024-04-29 11:14:41 +0200

    gsap cursor gradient animation

--- Vitrine/src/App.jsx
+++ Vitrine/src/App.jsx
@@ -1,14 +1,21 @@
 
 import Parallax from './components/Parallax'
+import BlurryCursor from './components/Cursor'
+import Scene1 from './components/Scene1';
+import Scene2 from './components/Scene2';
+import { useState } from 'react';
 import './App.css'
 
 function App() {
 
 
+  const [isActive, setIsActive] = useState(false);
   return (
-
     <div className='w-full h-full flex justify-center'>
-      <Parallax />
+      <Scene1 />
+      <Scene2 />
+      <BlurryCursor isActive={isActive} />
+      <Parallax onMouseOver={() => { setIsActive(true) }} onMouseLeave={() => { setIsActive(false) }} />
     </div>
 
   )
